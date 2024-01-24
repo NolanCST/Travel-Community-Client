8d5250c538aee827b30a52d7e250d3cc059d6d24
NolanCST <nolan.costa06@gmail.com>
2024-01-24 18:36:16 +0100

    bug fix

--- src/components/connexion/Login.jsx
+++ src/components/connexion/Login.jsx
@@ -2,61 +2,61 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
-   const [loginInput, setLogin] = useState({
-      pseudo: "",
-      password: "",
-      error_list: [],
-   });
-   const navigate = useNavigate();
+  const [loginInput, setLogin] = useState({
+    email: "",
+    password: "",
+    error_list: [],
+  });
+  const navigate = useNavigate();
 
-   const handleInput = (e) => {
-      e.preventDefault;
-      setLogin({ ...loginInput, [e.target.name]: e.target.value });
-   };
+  const handleInput = (e) => {
+    e.preventDefault;
+    setLogin({ ...loginInput, [e.target.name]: e.target.value });
+  };
 
-   const loginSubmit = async (e) => {
-      e.preventDefault();
+  const loginSubmit = async (e) => {
+    e.preventDefault();
 
-      let options = {
-         method: "POST",
-         headers: {
-            "Content-Type": "application/json",
-         },
-         body: JSON.stringify({
-            pseudo: loginInput.pseudo,
-            password: loginInput.password,
-         }),
-      };
+    let options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: loginInput.email,
+        password: loginInput.password,
+      }),
+    };
 
-      const response = await fetch(`${import.meta.env.VITE_API_URL}/login`, options);
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/login`, options);
 
-      const data = await response.json();
-      console.log(data);
-      if (data.status === 200) {
-         localStorage.setItem("@token", data.token);
-         localStorage.setItem("@pseudo", data.pseudo);
-         alert(data.message);
-         navigate("/");
-      } else if (data.status === 401) {
-         alert(data.message);
-      } else {
-         setLogin({ ...loginInput, error_list: data.validation_errors });
-      }
-   };
+    const data = await response.json();
+    console.log(data);
+    if (data.status === 200) {
+      localStorage.setItem("@token", data.token);
+      localStorage.setItem("@email", data.email);
+      alert(data.message);
+      navigate("/");
+    } else if (data.status === 401) {
+      alert(data.message);
+    } else {
+      setLogin({ ...loginInput, error_list: data.validation_errors });
+    }
+  };
 
-   return (
-      <>
-         <form onSubmit={loginSubmit}>
-            <label htmlFor="pseudo">Pseudo:</label>
-            <input id="pseudo" type="text" name="pseudo" value={loginInput.pseudo} onChange={handleInput} required />
-            <span>{loginInput.error_list.pseudo}</span>
-            <label htmlFor="password">Mot de passe:</label>
-            <input id="password" type="password" name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*_=+]).{12,}" value={loginInput.password} onChange={handleInput} required />
-            <span>{loginInput.error_list.password}</span>
-            <input type="submit" />
-         </form>
-      </>
-   );
+  return (
+    <>
+      <form onSubmit={loginSubmit}>
+        <label htmlFor="email">Email:</label>
+        <input id="email" type="email" name="email" value={loginInput.email} onChange={handleInput} required />
+        <span>{loginInput.error_list.email}</span>
+        <label htmlFor="password">Mot de passe:</label>
+        <input id="password" type="password" name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*_=+]).{12,}" value={loginInput.password} onChange={handleInput} required />
+        <span>{loginInput.error_list.password}</span>
+        <input type="submit" />
+      </form>
+    </>
+  );
 }
 
 export default Login;
