f91f37ca7d54e62fde9fe1c8bf0f20528d313961
NolanCST <nolan.costa06@gmail.com>
2024-01-27 17:14:26 +0100

    debut de la page home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,3 +1,17 @@
-function Home() {}
+import { useEffect } from "react";
+
+function Home() {
+  const getTravels = async () => {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/travels`);
+
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getTravels();
+  });
+}
 
 export default Home;
