d56d94f6fe593de99cd448b52b7deb00f9743c9d
NolanCST <nolan.costa06@gmail.com>
2024-01-29 11:58:51 +0100

    début de la page détail

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,33 +2,31 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function Home() {
-   const [travels, setTravels] = useState([]);
-   const getTravels = async () => {
-      const response = await fetch(`${import.meta.env.VITE_API_URL}/travels`);
+  const [travels, setTravels] = useState([]);
+  const getTravels = async () => {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/travels`);
 
-      const data = await response.json();
+    const data = await response.json();
 
-      setTravels(data.travels);
-   };
+    setTravels(data.travels);
+  };
 
-   useEffect(() => {
-      getTravels();
-   }, []);
+  useEffect(() => {
+    getTravels();
+  }, []);
 
-   const renderTravels = () => {
-      return travels?.map((element, index) => (
-         <Link key={index} to={`/details/${element.id}`} state={element.id} style={{ textDecoration: "none" }}>
-            <div>
-               <p>{element.title}</p>
-               <p>{element.country}</p>
-               <p>Jours: {element.days}</p>
-               <img src={element.image} alt="image" />
-            </div>
-         </Link>
-      ));
-   };
+  const renderTravels = () => {
+    return travels?.map((element, index) => (
+      <Link key={index} to={`/details/${element.id}`} state={element.id} style={{ textDecoration: "none" }}>
+        <p>{element.title}</p>
+        <p>{element.country}</p>
+        <p>Jours: {element.days}</p>
+        <img src={element.image} alt="image" />
+      </Link>
+    ));
+  };
 
-   return <div>{renderTravels()}</div>;
+  return <div>{renderTravels()}</div>;
 }
 
 export default Home;
--- src/components/travel/DetailsTravel.jsx
+++ src/components/travel/DetailsTravel.jsx
@@ -1,3 +1,37 @@
-function DetailsTravel() {}
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+function DetailsTravel() {
+  const travelId = useLocation().state;
+  const [travel, setTravel] = useState([]);
+
+  const getTravel = async () => {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/travels/${travelId}`);
+
+      const data = await response.json();
+      setTravel(data.travel);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTravel();
+  }, []);
+
+  const renderTravel = () => {
+    return travel?.map((element, index) => (
+      <div key={index}>
+        <p>{element.title}</p>
+        <p>{element.country}</p>
+        <p>Jours: {element.days}</p>
+        <img src={element.image} alt="image" />
+      </div>
+    ));
+  };
+
+  return <div>{renderTravel()}</div>;
+}
 
 export default DetailsTravel;
