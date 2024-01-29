e9f8e2d071ac75a173f577823b0e34303e48f852
NolanCST <nolan.costa06@gmail.com>
2024-01-29 18:03:45 +0100

    début de la page profile

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,37 +1,45 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Profile() {
-   const token = localStorage.getItem("@token");
-   async function getUser() {
-      try {
-         if (token) {
-            const response = await fetch(`${import.meta.env.VITE_API_URL}/user`, {
-               method: "GET",
-               headers: {
-                  "Content-Type": "application/json",
-                  Authorization: "Bearer " + token,
-               },
-            });
+  const [user, setUser] = useState([]);
+  const token = localStorage.getItem("@token");
+  async function getUser() {
+    try {
+      if (token) {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/user`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + token,
+          },
+        });
 
-            if (response.ok) {
-               const data = await response.json();
-
-               console.log(data);
-            } else {
-               console.error("Erreur dans la récupération de l'utilisateur");
-            }
-         } else {
-            console.error("Token non trouvé");
-         }
-      } catch (error) {
-         console.error("Erreur:", error);
+        if (response.ok) {
+          const data = await response.json();
+          setUser(data);
+        } else {
+          console.error("Erreur dans la récupération de l'utilisateur");
+        }
+      } else {
+        console.error("Token non trouvé");
       }
-   }
-   useEffect(() => {
-      getUser();
-   }, []);
+    } catch (error) {
+      console.error("Erreur:", error);
+    }
+  }
+  useEffect(() => {
+    getUser();
+  }, []);
 
-   return;
+  return (
+    <div>
+      <p>{user.lastname}</p>
+      <p>{user.firstname}</p>
+      <p>{user.pseudo}</p>
+      <p>{user.email}</p>
+      <p>{user.country}</p>
+    </div>
+  );
 }
 
 export default Profile;
