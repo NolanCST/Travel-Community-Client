dd571498a07b6a8daa0e399b0447c1906f00e81c
NolanCST <nolan.costa06@gmail.com>
2024-02-03 11:37:36 +0100

    possibilité de créer plusieurs jour pour un voyage

--- src/App.jsx
+++ src/App.jsx
@@ -7,43 +7,46 @@ import EditTravel from "./pages/travel/EditTravel";
 import Register from "./pages/connexion/Register";
 import Login from "./pages/connexion/Login";
 import Profile from "./pages/profile/profile";
+import { StatusProvider } from "./components/status/Status";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/create",
-      element: <CreateTravel />,
-    },
-    {
-      path: "/details/:id",
-      element: <DetailsTravel />,
-    },
-    {
-      path: "/edit/:id",
-      element: <EditTravel />,
-    },
-    {
-      path: "/register",
-      element: <Register />,
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/profile",
-      element: <Profile />,
-    },
-  ]);
-  return (
-    <>
-      <RouterProvider router={router} />
-    </>
-  );
+   const router = createBrowserRouter([
+      {
+         path: "/",
+         element: <Home />,
+      },
+      {
+         path: "/create",
+         element: <CreateTravel />,
+      },
+      {
+         path: "/details/:id",
+         element: <DetailsTravel />,
+      },
+      {
+         path: "/edit/:id",
+         element: <EditTravel />,
+      },
+      {
+         path: "/register",
+         element: <Register />,
+      },
+      {
+         path: "/login",
+         element: <Login />,
+      },
+      {
+         path: "/profile",
+         element: <Profile />,
+      },
+   ]);
+   return (
+      <>
+         <StatusProvider>
+            <RouterProvider router={router} />
+         </StatusProvider>
+      </>
+   );
 }
 
 export default App;
--- src/pages/travel/CreateTravel.jsx
+++ src/pages/travel/CreateTravel.jsx
@@ -4,101 +4,128 @@ import { useStatus } from "../../components/status/Status";
 import { useNavigate } from "react-router-dom";
 
 function CreateTravel() {
-  const { idUser } = useStatus;
-  const [formData, setFormData] = useState({
-    title: "",
-    description: "",
-    image: {},
-    days: "",
-    country: "",
-  });
-  const [country, setCountry] = useState("");
-  const [titleDay, setTitleDay] = useState([]);
-  const [descriptionDay, setDescriptionDay] = useState([]);
-  const [imageDay, setImageDay] = useState([]);
-  const navigate = useNavigate();
+   const { idUser } = useStatus();
+   const [formData, setFormData] = useState({
+      title: "",
+      description: "",
+      image: {},
+      days: "",
+      country: "",
+   });
+   const [country, setCountry] = useState("");
+   const [travelDays, setTravelDays] = useState([]);
 
-  const handleCountryChange = (selectedCountry) => {
-    setCountry(selectedCountry);
-    setFormData({ ...formData, country: country });
-  };
+   const [titleDay, setTitleDay] = useState([]);
+   const [descriptionDay, setDescriptionDay] = useState([]);
+   const [imageDay, setImageDay] = useState([]);
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+   const navigate = useNavigate();
 
-  const handleFile = (e) => {
-    const file = e.target.files[0];
-    setFormData({ ...formData, image: file });
-  };
+   const handleCountryChange = (selectedCountry) => {
+      setCountry(selectedCountry);
+      setFormData({ ...formData, country: selectedCountry });
+   };
 
-  const renderTravelDay = () => {
-    const travelDay = [];
-    for (let i = 0; i < formData.days; i++) {
-      travelDay.push(
-        <div key={i}>
-          <label htmlFor="title_day">Titre du jour</label>
-          <input type="text" id="title_day" name="title_day" max="50" value={titleDay} onChange={(e) => setTitleDay(e.target.value)} />
-          <label htmlFor="description_day">Description du jour</label>
-          <textarea name="description_day" id="description_day" cols="30" rows="10" value={descriptionDay} onChange={(e) => setDescriptionDay(e.target.value)}></textarea>
-          <label htmlFor="image_day">Images du jour</label>
-          <input type="file" id="image_day" name="image_day" onChange={(e) => setImageDay(e.target.value)} />
-        </div>
-      );
-    }
-    return travelDay;
-  };
+   const handleChange = (e) => {
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+   const handleFile = (e) => {
+      const file = e.target.files[0];
+      setFormData({ ...formData, image: file });
+   };
 
-    const formDataToSend = new FormData();
-    formDataToSend.append("title", formData.title);
-    formDataToSend.append("description", formData.description);
-    formDataToSend.append("image", formData.image);
-    formDataToSend.append("days", formData.days);
-    formDataToSend.append("country", formData.country);
-    formDataToSend.append("user_id", idUser);
+   const handleTitleDayChange = (index, value) => {
+      const updatedDays = [...travelDays];
 
-    try {
-      let options = {
-        method: "POST",
-        body: formDataToSend,
-      };
+      if (!updatedDays[index]) {
+         updatedDays[index] = {};
+      }
 
-      await fetch(`${import.meta.env.VITE_API_URL}/travels`, options)
-        .then((response) => {
-          if (response.ok) {
-            console.log(response);
-            // navigate("/profile");
-          } else {
-            throw new Error(`Erreur lors de la requête : ${response.status}`);
-          }
-        })
-        .catch((error) => console.error("Erreur lors de la requête :", error));
-    } catch (error) {
-      console.log(error);
-    }
-  };
+      updatedDays[index].titleDay = value;
+      setTravelDays(updatedDays);
+   };
 
-  return (
-    <>
-      <form encType="multipart/form-data" onSubmit={handleSubmit}>
-        <label htmlFor="title">Titre</label>
-        <input type="text" id="title" name="title" max="50" onChange={handleChange} required />
-        <label htmlFor="description">Description</label>
-        <textarea name="description" id="description" cols="30" rows="10" onChange={handleChange} required></textarea>
-        <label htmlFor="image">Image</label>
-        <input type="file" id="image" name="image" onChange={handleFile} />
-        <label htmlFor="days">Nombre de jours</label>
-        <input type="number" id="days" name="days" onChange={handleChange} required />
-        <label htmlFor="country">Destination</label>
-        <Country selectedCountry={country} onCountryChange={handleCountryChange} />
-        {renderTravelDay()}
-        <input type="submit" />
-      </form>
-    </>
-  );
+   const handleDescriptionDayChange = (index, value) => {
+      const updatedDays = [...travelDays];
+
+      if (!updatedDays[index]) {
+         updatedDays[index] = {};
+      }
+
+      updatedDays[index].descriptionDay = value;
+      setTravelDays(updatedDays);
+   };
+
+   const renderTravelDay = () => {
+      const travelDayElements = [];
+      for (let i = 0; i < formData.days; i++) {
+         travelDayElements.push(
+            <div key={i}>
+               <label htmlFor={`title_day_${i}`}>Titre du jour</label>
+               <input type="text" id={`title_day_${i}`} name={`title_day_${i}`} max="50" defaultValue={travelDays[i]?.titleDay || ""} onChange={(e) => handleTitleDayChange(i, e.target.value)} />
+               <label htmlFor={`description_day_${i}`}>Description du jour</label>
+               <textarea name={`description_day_${i}`} id={`description_day_${i}`} cols="30" rows="10" defaultValue={travelDays[i]?.descriptionDay || ""} onChange={(e) => handleDescriptionDayChange(i, e.target.value)}></textarea>
+               <label htmlFor="image_day">Images du jour</label>
+               <input type="file" id="image_day" name="image_day" onChange={(e) => setImageDay(e.target.value)} />
+            </div>
+         );
+      }
+      return travelDayElements;
+   };
+
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+
+      const formDataToSend = new FormData();
+      formDataToSend.append("title", formData.title);
+      formDataToSend.append("description", formData.description);
+      formDataToSend.append("image", formData.image);
+      formDataToSend.append("days", formData.days);
+      formDataToSend.append("country", formData.country);
+      formDataToSend.append("user_id", idUser);
+      console.log(travelDays);
+      formDataToSend.append("travelDays", JSON.stringify(travelDays));
+
+      try {
+         let options = {
+            method: "POST",
+            body: formDataToSend,
+         };
+
+         await fetch(`${import.meta.env.VITE_API_URL}/travels`, options)
+            .then((response) => {
+               if (response.ok) {
+                  console.log(response);
+                  // navigate("/profile");
+               } else {
+                  throw new Error(`Erreur lors de la requête : ${response.status}`);
+               }
+            })
+            .catch((error) => console.error("Erreur lors de la requête :", error));
+      } catch (error) {
+         console.log(error);
+      }
+   };
+
+   return (
+      <>
+         <form encType="multipart/form-data" onSubmit={handleSubmit}>
+            <label htmlFor="title">Titre</label>
+            <input type="text" id="title" name="title" max="50" onChange={handleChange} required />
+            <label htmlFor="description">Description</label>
+            <textarea name="description" id="description" cols="30" rows="10" onChange={handleChange} required></textarea>
+            <label htmlFor="image">Image</label>
+            <input type="file" id="image" name="image" onChange={handleFile} />
+            <label htmlFor="days">Nombre de jours</label>
+            <input type="number" id="days" name="days" onChange={handleChange} required />
+            <label htmlFor="country">Destination</label>
+            <Country selectedCountry={country} onCountryChange={handleCountryChange} />
+            {renderTravelDay()}
+            <input type="submit" />
+         </form>
+      </>
+   );
 }
 
 export default CreateTravel;
