33531ad29bd58d3eec4e2f16cdc071b93ee1324e
NolanCST <nolan.costa06@gmail.com>
2024-02-07 17:37:18 +0100

    debut de la navbar

--- src/components/layouts/NavBar.jsx
+++ src/components/layouts/NavBar.jsx
@@ -1,3 +1,105 @@
-function NavBar() {}
+import { useStatus } from "../status/Status";
+import { useNavigate } from "react-router-dom";
+import "./navbar.css";
+import { useState } from "react";
+// import logo from "./Corgi_guge-removebg-preview.png";
 
-export default NavBar;
+function Navbar() {
+   const { status } = useStatus();
+   const navigate = useNavigate();
+   const [isOpen, setIsOpen] = useState(false);
+
+   const toggleMenu = () => {
+      setIsOpen(!isOpen);
+   };
+   async function handleLogout() {
+      const isConfirmed = window.confirm("Êtes-vous sûr de vouloir vous déconnecter ?");
+
+      if (isConfirmed) {
+         // Supprime les infos de connexion du localStorage
+         localStorage.removeItem("email");
+         localStorage.removeItem("password");
+         localStorage.removeItem("token");
+
+         try {
+            // Requête vers API pour révoquer le token
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/logout`, {
+               method: "POST",
+               headers: {
+                  "Content-Type": "application/json",
+               },
+            });
+
+            if (response.ok) {
+               // Redirige l'utilisateur vers la page de connexion
+               navigate("/Login");
+            }
+         } catch (error) {
+            console.error("Erreur lors de la déconnexion côté serveur :", error);
+         }
+      }
+   }
+
+   return (
+      <div>
+         {/* Menu Hamburger pour les petits écrans */}
+         <div className="hamburgerMenu" onClick={toggleMenu}>
+            <div className={isOpen ? "line line1 active" : "line line1"}></div>
+            <div className={isOpen ? "line line2 active" : "line line2"}></div>
+            <div className={isOpen ? "line line3 active" : "line line3"}></div>
+         </div>
+         <nav className={`navTag ${isOpen ? "open" : ""}`}>
+            <a className="imageLink" href="/">
+               {/* <img className="logoSmall" src={logo} /> */}
+            </a>
+            <div className="navbar">
+               <ul className="navbarListLeft">
+                  <li>
+                     <a className="link" href="/">
+                        Accueil
+                     </a>
+                  </li>
+                  {(status === 0 || status === 1) && (
+                     <li>
+                        <a className="link" href="/profile">
+                           Profil
+                        </a>
+                     </li>
+                  )}
+                  {status === 1 && (
+                     <li>
+                        <a className="link" href="/create">
+                           Ajouter
+                        </a>
+                     </li>
+                  )}
+               </ul>
+               <ul className="navbarListRight">
+                  {status === 0 || status === 1 ? (
+                     <li>
+                        <a className="link" onClick={handleLogout}>
+                           Déconnexion
+                        </a>
+                     </li>
+                  ) : (
+                     <>
+                        <li>
+                           <a className="link" href="/register">
+                              Inscription
+                           </a>
+                        </li>
+                        <li>
+                           <a className="link" href="/login">
+                              Connexion
+                           </a>
+                        </li>
+                     </>
+                  )}
+               </ul>
+            </div>
+         </nav>
+      </div>
+   );
+}
+
+export default Navbar;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,32 +1,40 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Navbar from "../../components/layouts/NavBar";
 
 function Home() {
-  const [travels, setTravels] = useState([]);
-  const getTravels = async () => {
-    const response = await fetch(`${import.meta.env.VITE_API_URL}/travels`);
+   const [travels, setTravels] = useState([]);
+   const getTravels = async () => {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/travels`);
 
-    const data = await response.json();
+      const data = await response.json();
 
-    setTravels(data.travels);
-  };
+      setTravels(data.travels);
+   };
 
-  useEffect(() => {
-    getTravels();
-  }, []);
+   useEffect(() => {
+      getTravels();
+   }, []);
 
-  const renderTravels = () => {
-    return travels?.map((element, index) => (
-      <Link key={index} to={`/details/${element.id}`} state={element.id} style={{ textDecoration: "none" }}>
-        <p>{element.title}</p>
-        <p>{element.country}</p>
-        <p>Jours: {element.days}</p>
-        <img src={element.image} alt="image" />
-      </Link>
-    ));
-  };
+   const renderTravels = () => {
+      return travels?.map((element, index) => (
+         <Link key={index} to={`/details/${element.id}`} state={element.id} style={{ textDecoration: "none" }}>
+            <p>{element.title}</p>
+            <p>{element.country}</p>
+            <p>Jours: {element.days}</p>
+            <img src={element.image} alt="image" />
+         </Link>
+      ));
+   };
 
-  return <div>{renderTravels()}</div>;
+   return (
+      <>
+         <nav>
+            <Navbar />
+         </nav>
+         <div>{renderTravels()}</div>
+      </>
+   );
 }
 
 export default Home;
