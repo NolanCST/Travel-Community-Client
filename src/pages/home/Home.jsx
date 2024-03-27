24eb4de5bc2fff8de13ca04439c8a22a6158767a
NolanCST <nolan.costa06@gmail.com>
2024-03-27 09:07:44 +0100

    ajout de texte

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,122 +6,124 @@ import "./home.css";
 import Footer from "../../components/layouts/Footer";
 
 function Home() {
-  const { status } = useStatus();
-  const [travels, setTravels] = useState([]);
-  const navigate = useNavigate();
+   const { status } = useStatus();
+   const [travels, setTravels] = useState([]);
+   const navigate = useNavigate();
 
-  const getTravels = async () => {
-    const response = await fetch(`${import.meta.env.VITE_API_URL}/travels`);
+   const getTravels = async () => {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/travels`);
 
-    const data = await response.json();
+      const data = await response.json();
 
-    setTravels(data.travels);
-  };
+      setTravels(data.travels);
+   };
 
-  useEffect(() => {
-    getTravels();
-  }, []);
+   useEffect(() => {
+      getTravels();
+   }, []);
 
-  const renderTravels = () => {
-    return travels?.map((element, index) => (
-      <Link className="traCard" key={index} to={`/details/${element.id}`} state={element.id} style={{ textDecoration: "none" }}>
-        <img className="cardImage" src={element.image} alt="image" />
-        <div className="cardInfos">
-          <h3 className="cardTitle">{element.title}</h3>
-          <p className="cardCountry">Pays: {element.legislations[0].country}</p>
-          <p className="cardDays">Nombre de jours: {element.days}</p>
-        </div>
-        <a className="homeBtn" href="#">
-          VOIR
-        </a>
-      </Link>
-    ));
-  };
-
-  const goCreate = () => {
-    if (status !== null) {
-      navigate("/create");
-    } else {
-      navigate("/login");
-    }
-  };
-
-  return (
-    <>
-      <nav>
-        <Navbar />
-      </nav>
-      <div className="welcome">
-        <video className="welVideo" autoPlay loop muted>
-          <source src="../../public/videos/fond-accueil.mp4" type="video/mp4" />
-        </video>
-        <div className="company">
-          <img className="logo" src="../../public/images/logo.png" alt="logo" />
-          <h1 className="name">Travel Community</h1>
-        </div>
-        <p className="welTitle">
-          C'est l'heure de votre <strong>prochaine aventure</strong>
-        </p>
-        <a className="homeBtn" href="#">
-          DÉCOUVRIR
-        </a>
-      </div>
-
-      <div className="presentation">
-        <div className="part1">
-          <div className="part1-top">
-            <img className="logo" src="../../public/images/logo.png" alt="logo" />
-            <h1 className="preTitle">Voyager facilement</h1>
-          </div>
-          <p className="preDescription">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam optio deserunt porro in quo temporibus expedita velit, qui et deleniti iste laudantium sequi cupiditate nemo beatae, corporis maiores dicta. Commodi?
-          </p>
-          <div className="preBtn">
+   const renderTravels = () => {
+      return travels?.map((element, index) => (
+         <Link className="traCard" key={index} to={`/details/${element.id}`} state={element.id} style={{ textDecoration: "none" }}>
+            <img className="cardImage" src={element.image} alt="image" />
+            <div className="cardInfos">
+               <h3 className="cardTitle">{element.title}</h3>
+               <p className="cardCountry">Pays: {element.legislations[0].country}</p>
+               <p className="cardDays">Nombre de jours: {element.days}</p>
+            </div>
             <a className="homeBtn" href="#">
-              TOUS LES VOYAGES
+               VOIR
             </a>
-            <button className="homeBtn" onClick={goCreate}>
-              PARTAGER LE VOTRE
-            </button>
-          </div>
-        </div>
-        <img className="preImage" src="../../public/images/presentation.png" alt="voyageuse" />
-      </div>
+         </Link>
+      ));
+   };
 
-      <div className="travels">
-        <h2 className="traTitle">Derniers Voyages</h2>
-        <div className="renderTravel">{renderTravels()}</div>
-      </div>
+   const goCreate = () => {
+      if (status !== null) {
+         navigate("/create");
+      } else {
+         navigate("/login");
+      }
+   };
 
-      <div className="advantages">
-        <h2 className="advTitle">Le voyage parfait à porter de main</h2>
-        <div className="advAllInfo">
-          <div className="advInfo">
-            <img className="infoImage" src="../../public/images/plane.png" alt="plane" />
-            <h3 className="infoTitle">Transports</h3>
-            <p className="infoDescription">Partagez vos astuces pour des déplacements fluides et offrez à d'autres la clé d'une aventure sans encombre.</p>
-          </div>
-          <div className="advInfo">
-            <img className="infoImage" src="../../public/images/woman.png" alt="woman" />
-            <h3 className="infoTitle">Visites & activités</h3>
-            <p className="infoDescription">Proposez des idées captivantes pour permettre à d'autres de vivre des moments mémorables lors de leurs propres escapades.</p>
-          </div>
-          <div className="advInfo">
-            <img className="infoImage" src="../../public/images/hotel.png" alt="hotel" />
-            <h3 className="infoTitle">Hébergements</h3>
-            <p className="infoDescription">Guidez la communauté vers des séjours inoubliables à travers vos recommandations personnelles.</p>
-          </div>
-        </div>
-        <a className="homeBtn" href="#">
-          DÉCOUVRIR
-        </a>
-      </div>
+   return (
+      <>
+         <nav>
+            <Navbar />
+         </nav>
+         <div className="welcome">
+            <video className="welVideo" autoPlay loop muted>
+               <source src="../../public/videos/fond-accueil.mp4" type="video/mp4" />
+            </video>
+            <div className="company">
+               <img className="logo" src="../../public/images/logo.png" alt="logo" />
+               <h1 className="name">Travel Community</h1>
+            </div>
+            <p className="welTitle">
+               C'est l'heure de votre <strong>prochaine aventure</strong>
+            </p>
+            <a className="homeBtn" href="#">
+               DÉCOUVRIR
+            </a>
+         </div>
 
-      <footer>
-        <Footer />
-      </footer>
-    </>
-  );
+         <div className="presentation">
+            <div className="part1">
+               <div className="part1-top">
+                  <img className="logo" src="../../public/images/logo.png" alt="logo" />
+                  <h1 className="preTitle">Voyager facilement</h1>
+               </div>
+               <p className="preDescription">
+                  Découvrir de nouveaux horizons, explorer des contrées lointaines, et s'émerveiller devant la diversité du monde. Voyager facilement, c'est ouvrir les portes de l'aventure et s'immerger dans des cultures riches en histoire et en
+                  tradition. Que ce soit par les airs, sur terre ou sur mer, l'appel de l'exploration nous pousse à parcourir des kilomètres, à dépasser les frontières et à embrasser la beauté infinie de notre planète. Chaque voyage est une
+                  expérience unique, une opportunité de grandir, d'apprendre et de se connecter avec le monde qui nous entoure. Alors, laissez-vous emporter par la magie du voyage et laissez vos souvenirs se remplir de moments inoubliables.{" "}
+               </p>
+               <div className="preBtn">
+                  <a className="homeBtn" href="#">
+                     TOUS LES VOYAGES
+                  </a>
+                  <button className="homeBtn" onClick={goCreate}>
+                     PARTAGER LE VOTRE
+                  </button>
+               </div>
+            </div>
+            <img className="preImage" src="../../public/images/presentation.png" alt="voyageuse" />
+         </div>
+
+         <div className="travels">
+            <h2 className="traTitle">Derniers Voyages</h2>
+            <div className="renderTravel">{renderTravels()}</div>
+         </div>
+
+         <div className="advantages">
+            <h2 className="advTitle">Le voyage parfait à porter de main</h2>
+            <div className="advAllInfo">
+               <div className="advInfo">
+                  <img className="infoImage" src="../../public/images/plane.png" alt="plane" />
+                  <h3 className="infoTitle">Transports</h3>
+                  <p className="infoDescription">Partagez vos astuces pour des déplacements fluides et offrez à d'autres la clé d'une aventure sans encombre.</p>
+               </div>
+               <div className="advInfo">
+                  <img className="infoImage" src="../../public/images/woman.png" alt="woman" />
+                  <h3 className="infoTitle">Visites & activités</h3>
+                  <p className="infoDescription">Proposez des idées captivantes pour permettre à d'autres de vivre des moments mémorables lors de leurs propres escapades.</p>
+               </div>
+               <div className="advInfo">
+                  <img className="infoImage" src="../../public/images/hotel.png" alt="hotel" />
+                  <h3 className="infoTitle">Hébergements</h3>
+                  <p className="infoDescription">Guidez la communauté vers des séjours inoubliables à travers vos recommandations personnelles.</p>
+               </div>
+            </div>
+            <a className="homeBtn" href="#">
+               DÉCOUVRIR
+            </a>
+         </div>
+
+         <footer>
+            <Footer />
+         </footer>
+      </>
+   );
 }
 
 export default Home;
